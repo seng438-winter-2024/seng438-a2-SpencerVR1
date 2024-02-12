90c6f4f0e37b3485a1ddbc032f5e43c939528e47
HoussemZ4 <114112576+HoussemZ4@users.noreply.github.com>
2024-02-11 23:46:06 -0700

    Update DataUtilities345Test.java

--- data_utilties_methods345/DataUtilities345Test.java
+++ data_utilties_methods345/DataUtilities345Test.java
@@ -10,6 +10,263 @@ import org.jmock.*;
 
 public class DataUtilities345Test extends DataUtilities {
 	
+	 // This test covers valid input for the column index argument and valid
+	 // input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithValidInput() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getRowCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(1));
+				 one(values).getValue(1, 0); 
+				 will(returnValue(2));
+				 one(values).getValue(2, 0); 
+				 will(returnValue(3));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateColumnTotal(values, 0);
+		 // verify
+		 assertEquals("Column total should be 6", 6.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers invalid, out of bounds, input for the column index argument 
+	 // and valid input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithOutOfBoundsIndex() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getRowCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(1));
+				 one(values).getValue(1, 0); 
+				 will(returnValue(2));
+				 one(values).getValue(2, 0); 
+				 will(returnValue(3));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateColumnTotal(values, 1);
+		 // verify
+		 assertEquals("Column total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the column index argument and valid, all
+	 // zero input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithAllZeroValues() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getRowCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(0));
+				 one(values).getValue(1, 0); 
+				 will(returnValue(0));
+				 one(values).getValue(2, 0); 
+				 will(returnValue(0));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateColumnTotal(values, 0);
+		 // verify
+		 assertEquals("Column total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the column index argument and valid, mixed
+	 // input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithMixedValues() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getRowCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(-2));
+				 one(values).getValue(1, 0); 
+				 will(returnValue(0));
+				 one(values).getValue(2, 0); 
+				 will(returnValue(2));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateColumnTotal(values, 0);
+		 // verify
+		 assertEquals("Column total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the column index argument and invalid
+	 // input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithInvalidInput() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getRowCount();
+				 will(returnValue(0));
+			 }
+		 });
+		 double result = DataUtilities.calculateColumnTotal(values, 0);
+		 // verify
+		 assertEquals("Column total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test checks if InvalidParameterException is thrown with invalid, null
+         // input for the data argument in method calculateColumnTotal(data, index)
+	 @Test
+	 public void calculateColumnTotalWithInvalidDataObject() {
+	    try {
+	    	DataUtilities.calculateColumnTotal(null, 0); 
+	    	fail("Expected InvalidParameterException to be thrown");
+	    } catch (NullPointerException e) {
+	        fail("Unexpected NullPointerException occurred");
+	    } catch (InvalidParameterException e) {
+	    	// Expected behavior
+	    }
+	 }
+	 
+	 // This test covers valid input for the row index argument and valid
+         // input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithValidInput() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getColumnCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(1));
+				 one(values).getValue(0, 1); 
+				 will(returnValue(2));
+				 one(values).getValue(0, 2); 
+				 will(returnValue(3));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateRowTotal(values, 0);
+		 // verify
+		 assertEquals("Row total should be 6", 6.0, result, .000000001d); 
+	 }
+	 
+	 
+	 // This test covers invalid, out of bounds, input for the row index argument 
+	 // and valid input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithOutOfBoundsIndex() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getColumnCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(1));
+				 one(values).getValue(0, 1); 
+				 will(returnValue(2));
+				 one(values).getValue(0, 2); 
+				 will(returnValue(3));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateRowTotal(values, 1);
+		 // verify
+		 assertEquals("Row total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the row index argument and valid, all
+	 // zero input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithAllZeroValues() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getColumnCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(0));
+				 one(values).getValue(0, 1); 
+				 will(returnValue(0));
+				 one(values).getValue(0, 2); 
+				 will(returnValue(0));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateRowTotal(values, 0);
+		 // verify
+		 assertEquals("Row total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the row index argument and valid, mixed
+	 // input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithMixedValues() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getColumnCount(); 
+				 will(returnValue(3));
+				 one(values).getValue(0, 0);
+				 will(returnValue(-2));
+				 one(values).getValue(0, 1); 
+				 will(returnValue(0));
+				 one(values).getValue(0, 2); 
+				 will(returnValue(2));	  
+			 }
+		 });
+		 double result = DataUtilities.calculateRowTotal(values, 0);
+		 // verify
+		 assertEquals("Row total should be 0", 0.0, result, .000000001d); 
+	 }
+	 
+	 // This test covers valid input for the row index argument and invalid
+	 // input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithInvalidInput() {
+		 // setup
+		 Mockery context = new Mockery();
+		 final Values2D values = context.mock(Values2D.class);
+		 context.checking(new Expectations() {
+			 {
+				 one(values).getColumnCount();
+				 will(returnValue(0));
+			 }
+		 });
+		 double result = DataUtilities.calculateRowTotal(values, 0);
+		 // verify
+		 assertEquals("Row total should be 0", 0.0, result, .000000001d); 
+	 } 
+	 
+	 // This test checks if InvalidParameterException is thrown with invalid, null
+         // input for the data argument in method calculateRowTotal(data, index)
+	 @Test
+	 public void calculateRowTotalWithInvalidDataObject() {
+	    try {
+	    	DataUtilities.calculateRowTotal(null, 0); 
+	    	fail("Expected InvalidParameterException to be thrown");
+	    } catch (NullPointerException e) {
+	        fail("Unexpected NullPointerException occurred");
+	    } catch (InvalidParameterException e) {
+	    	// Expected behavior
+	    }
+	 }
+
 	
 	@Test
 	public void testCreateNumberArray() {
